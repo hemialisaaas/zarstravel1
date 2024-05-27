9ae4576ca3cb785132414dfb516f152f394f2db5
fadhilmuhamaadd <121470307+fadhilmuhamaadd@users.noreply.github.com>
2024-05-27 19:40:58 +0700

    travelpackage

--- routes/web.php
+++ routes/web.php
@@ -31,7 +31,7 @@ Route::prefix('admin')
         Route::get('/', 'App\Http\Controllers\Admin\DashboardController@index')
         ->name('dashboard');
 
-        Route::resource('travel-package', 'App\Http\Controllers\Admin\TravelPackageController');
+        Route::resource('travelpackage', 'App\Http\Controllers\Admin\TravelPackageController');
      });
 
 Auth::routes(['verify' => true]);
