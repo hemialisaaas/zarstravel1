f246c1d6e8b1aa61f16355bc241fa417b70e0018
fadhilmuhamaadd <121470307+fadhilmuhamaadd@users.noreply.github.com>
2024-05-28 09:45:11 +0700

    done

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -10,35 +10,14 @@ class Transaction extends Model
     use SoftDeletes;
 
     protected $fillable = [
-<<<<<<< HEAD
-       'travel_package_id', 'user_id', 'additional_visa',
-       'transaction_total', 'transaction_status'
-
-=======
         'travel_package_id','user_id','additional_visa',
         'transaction_total','transaction_status'
->>>>>>> a29f9aa3779219e363600612a78fcc351ed7b861
     ];
 
     protected $hidden =[
 
     ];
 
-<<<<<<< HEAD
-    public function details()
-    {
-        return $this->hasMany(TransactionDetail::class, 'transaction_id', 'id');
-    }
-
-    public function travel_package()
-    {
-        return $this->belongsTo(TravelPackage::class, 'travel_package_id', 'id');
-    }
-
-    public function user()
-    {
-        return $this->belongsTo(User::class, 'user_id', 'id');
-=======
     public function details(){
         return $this->hasMany(TransactionDetail::class,'transaction_id','id');
     }
@@ -48,6 +27,5 @@ class Transaction extends Model
     }
     public function user(){
         return $this->belongsTo(User::class,'user_id','id');
->>>>>>> a29f9aa3779219e363600612a78fcc351ed7b861
     }
 }
--- app/Models/TransactionDetail.php
+++ app/Models/TransactionDetail.php
@@ -5,37 +5,18 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-<<<<<<< HEAD
-class TransactionDetail extends Model
-=======
 class TranscsactionDetail extends Model
->>>>>>> a29f9aa3779219e363600612a78fcc351ed7b861
 {
     use SoftDeletes;
 
     protected $fillable = [
-<<<<<<< HEAD
-
-        'transaction_id', 'user_id', 'travel_package_id', 'is_visa', 'doe_passport'
-
-=======
         'transaction_id','username','nationality','is_visa','doe_passport',
->>>>>>> a29f9aa3779219e363600612a78fcc351ed7b861
     ];
 
     protected $hidden =[
 
     ];
-
-<<<<<<< HEAD
-    public function transaction()
-    {
-        return $this->belongsTo(Transaction::class, 'transaction_id', 'id');
-    }
-
-=======
     public function transaction(){
         return $this->belongsTo(Transaction::class,'transaction_id','id');
     }
->>>>>>> a29f9aa3779219e363600612a78fcc351ed7b861
 }
